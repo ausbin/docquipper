3e14a4cf3c9f09e41f9084259366c6e53c486e06
Austin Adams <git@austinjadams.com>
2024-08-12 02:04:12 -0400

    Support additional args to the quipper program

--- qasm.sh
+++ qasm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-if [[ $# -ne 1 ]]; then
+if [[ $# -lt 1 ]]; then
     printf 'usage: %s file.hs\n' "$0" >&2
     exit 1
 fi
 file=$1
-quipper $file
-QasmPrinting <(./${file%.hs}) > ${file%.hs}.qasm
+shift
+quipper "$file"
+QasmPrinting <("./${file%.hs}" "$@") > "${file%.hs}".qasm
 printf 'done writing to %s\n' "${file%.hs}.qasm"
